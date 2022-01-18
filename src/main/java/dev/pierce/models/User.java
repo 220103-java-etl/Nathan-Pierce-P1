9ce9ad39f3dc0d324bcb8f83511246208a8b5ee6
Nathan Pierce <nathan.pheonixgamer@outlook.com>
2022-01-18 16:59:12 -0600

    Added User Class Model

---
+++ src/main/java/dev/pierce/models/User.java
@@ -0,0 +1,60 @@
+package dev.pierce.models;
+
+public class User {
+
+    // first and last name, username, password, id
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private int id;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
