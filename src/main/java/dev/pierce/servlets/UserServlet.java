220bf049677a1b9cf28ac69808a1a46f1b8dd999
Nathan Pierce <nathan.pheonixgamer@outlook.com>
2022-02-03 22:50:04 -0600

    Login functionality works

--- src/main/java/dev/pierce/servlets/UserServlet.java
+++ src/main/java/dev/pierce/servlets/UserServlet.java
@@ -2,25 +2,22 @@ package dev.pierce.servlets;
 
 import dev.pierce.models.Role;
 import dev.pierce.models.User;
-import dev.pierce.repository.UserDAO;
 import dev.pierce.services.UserService;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 public class UserServlet extends HttpServlet {
 
     ObjectMapper objectMapper = new ObjectMapper();
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        UserDAO userDAO = new UserDAO();
         UserService userService = new UserService();
 
         String username = request.getParameter("username");
@@ -30,8 +27,9 @@ public class UserServlet extends HttpServlet {
 
 
        if (u != null){
-
-            //redirect based on role
+           HttpSession session=request.getSession();
+           session.setAttribute("currentUser", u);
+           //redirect based on role
             if(u.getRole().equals(Role.FINANCE_MANAGER)){
                 response.sendRedirect("manager.html");
             }
@@ -45,4 +43,14 @@ public class UserServlet extends HttpServlet {
 
     }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+
+        if(session != null){
+            User user = (User)  session.getAttribute("currentUser");
+            response.getWriter().write(objectMapper.writeValueAsString(user));
+        }
+
+    }
 }
--- src/main/webapp/employee.js
+++ src/main/webapp/employee.js
@@ -1,10 +1,25 @@
 window.onload = function() {
     document.getElementById('Reimbursements').style.display = 'none';
     document.getElementById('submitForm').style.display = 'none';
+
+    let xhr = new XMLHttpRequest()
+    let url = "http://localhost:8080/Project-1/login";
+    xhr.open("GET", url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readystate == 4 && xhr.status == 200 ){
+            let jsonRes = JSON.parse(req.responseText);
+            localStorage.setItem("UsersName", jsonRes.fullName);
+            document.getElementById('Name').innerHTML =localStorage.getItem("UsersName");
+        }
+    }
+        
+ xhr.send();
 };
+
+
 
 function viewRequests(){
     document.getElementById('Reimbursements').style.display= 'block';
 
-    
+
 }
